66fe7cc819be8b28450295e725f83efed40507a7
Evan Bagwell <etb@wi.rr.com>
2020-06-24 15:18:32 -0500

    Create Page - View - Navigation

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CreateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CTWMasterClass_WebAppActivities.Controllers
+{
+    public class CreateController : Controller
+    {
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
