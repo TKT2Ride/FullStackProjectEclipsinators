b31c15477c2c20d00e44e672f00bca844044baf8
Artie Krishnan <artiekrishnan@gmail.com>
2020-06-25 09:14:22 -0500

    HTMl

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/HomeController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/HomeController.cs
@@ -8,22 +8,8 @@ namespace CTWMasterClass_WebAppActivities.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult HomePage()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
