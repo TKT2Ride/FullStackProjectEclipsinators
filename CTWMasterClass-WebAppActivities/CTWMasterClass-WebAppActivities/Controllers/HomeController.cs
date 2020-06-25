6fd48903cf5b7fc26e042254e2cf0f664a36c280
Artie Krishnan <artiekrishnan@gmail.com>
2020-06-25 12:47:32 -0500

    Changes pt2

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/HomeController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/HomeController.cs
@@ -8,8 +8,22 @@ namespace CTWMasterClass_WebAppActivities.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult HomePage()
+        public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
     }
--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Models/Barrel.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Models/Barrel.cs
@@ -8,8 +8,9 @@ namespace CTWMasterClass_WebAppActivities.Models
     public class Barrel
     {
         public int Id { get; set; }
-        public double Radius { get; set;  }
+        public double Radius { get; set; }
         public double Height { get; set; }
+        public double Weight { get; set; }
         public string ConstructionMaterial { get; set; }
         public string Contents { get; set; }
         public string CurrentLocation { get; set; }
