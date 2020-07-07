192fc1ec92dad6ac25ae4fcc64b7d77a4ec9ba59
Evan Bagwell <etb@wi.rr.com>
2020-07-07 11:34:21 -0500

    Create Cube Progress

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CubeController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CubeController.cs
@@ -3,31 +3,37 @@ using CTWMasterClass_WebAppActivities.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
 
 namespace CTWMasterClass_WebAppActivities.Controllers
 {
+   
     public class CubeController : Controller
-    {
+    { 
+        private CubeService serviceCube = new CubeService();
         // GET: Cube
         public ActionResult Index()
         {
+            return View(serviceCube.GetAllCubes());
+        }
+        public ActionResult CreateCube()
+        {
             return View();
         }
-        public ActionResult Details()
+        public ActionResult Details(int? Id)
         {
             if (Id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Cube Cube = service.GetBarrelById((int)Id);
-            if (barrel == null)
+            Cube cube = serviceCube.GetCubeById((int)Id);
+            if (cube == null)
             {
                 return HttpNotFound();
             }
-            return View(barrel);
+            return View(cube);
         }
     }
 }
