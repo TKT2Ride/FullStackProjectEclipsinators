f2a33fcbe1bb4f65a8149052ecc27bb06a02c292
Rebecca Gilpatric <rebeccagilpatric@gmail.com>
2020-07-07 10:33:16 -0500

    Back end stuff

---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Repositories/CubeRepository.cs
@@ -0,0 +1,42 @@
+﻿using CTWMasterClass_WebAppActivities.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CTWMasterClass_WebAppActivities.Repositories
+{
+    public class CubeRepository
+    {
+        private ApplicationDbContext dbContext;
+
+        public CubeRepository()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        public List<Cube> GetAllCubes()
+        {
+            return dbContext.Cubes.ToList();
+        }
+        public void AddCube(Cube toAdd)
+        {
+            dbContext.Cubes.Add(toAdd);
+            dbContext.SaveChanges();
+        }
+        public Cube GetCubeById(int Id)
+        {
+            return dbContext.Cubes.Find(Id);
+        }
+        public void DeleteCube(Cube toDelete)
+        {
+            dbContext.Cubes.Remove(toDelete);
+            dbContext.SaveChanges();
+        }
+        public void SaveEdits(Cube toSave)
+        {
+            dbContext.Entry(toSave).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Service/CubeService.cs
@@ -0,0 +1,41 @@
+﻿using CTWMasterClass_WebAppActivities.Models;
+using CTWMasterClass_WebAppActivities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CTWMasterClass_WebAppActivities.Service
+{
+    public class CubeService
+    {
+        private CubeRepository repository;
+
+        public CubeService()
+        {
+            repository = new CubeRepository();
+        }
+
+        public List<Cube> GetAllCubes()
+        {
+            return repository.GetAllCubes();
+        }
+        public void AddCube(Cube toAdd)
+        {
+            repository.AddCube(toAdd);
+        }
+        public void SaveEdits(Cube toSave)
+        {
+            repository.SaveEdits(toSave);
+        }
+        public void DeleteCube(Cube cube)
+        {
+            repository.DeleteCube(cube);
+        }
+        public Barrel GetCubeById(int Id)
+        {
+            return repository.GetCubeById(Id);
+        }
+    }
+}
