70e94c1c4f115c995013e2992034c05dd3da80cd
Artie Krishnan <artiekrishnan@gmail.com>
2020-07-07 11:46:17 -0500

    Home Page and Details

--- CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CubeController.cs
+++ CTWMasterClass-WebAppActivities/CTWMasterClass-WebAppActivities/Controllers/CubeController.cs
@@ -3,6 +3,7 @@ using CTWMasterClass_WebAppActivities.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
@@ -11,23 +12,28 @@ namespace CTWMasterClass_WebAppActivities.Controllers
 {
     public class CubeController : Controller
     {
+        private CubeService service = new CubeService();
         // GET: Cube
         public ActionResult Index()
         {
-            return View();
+            return View(service.GetAllCubes());
         }
-        public ActionResult Details()
+        public ActionResult Details(int? Id)
         {
             if (Id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Cube Cube = service.GetBarrelById((int)Id);
-            if (barrel == null)
+            Cube Cube = service.GetCubeById((int)Id);
+            if (Cube == null)
             {
                 return HttpNotFound();
             }
-            return View(barrel);
+            return View(Cube);
+        }
+        public ActionResult CubeHomePage()
+        {
+            return View();
         }
     }
 }
